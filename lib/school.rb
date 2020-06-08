39ee59776808cc97ac30e3df00dd4e919ec9feb0
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-06-08 00:33:18 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+
+def initialize(name)
+  @name=name
+  @roster = {}
+     end
+  def roster
+    @roster
+  end
+  def add_student(name,grade)
+   if @roster.has_key?(grade) then
+     @roster[grade]<< name
+   else 
+   @roster [grade] = []
+   @roster[grade]<< name
+  end
+end
+
+def grade(grade)
+  @roster[grade].select { |value|  value = grade}
+end
+
+def sort
+  
+@roster.each do  |key, value| @roster[key]= value.sort end
+
+end
+end
+
